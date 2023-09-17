cd853aa138d1606e43b6f7e934cbee9f7a9704f1
Jose Chirivella <jrchg1403@gmail.com>
2023-09-16 21:31:14 -0500

    contact goes to the footer

--- components/Nav.vue
+++ components/Nav.vue
@@ -51,7 +51,7 @@
           <li class="nav-item">
             <NuxtLink
               class="px-3 py-2 flex items-center text-lg uppercase font-bold leading-snug text-white hover:opacity-75"
-              to="/contact"
+              to="/#contact-home"
             >
               <i
                 class="fab fa-pinterest text-lg leading-lg text-white opacity-75"
